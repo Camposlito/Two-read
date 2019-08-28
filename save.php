480c246c92a26493db23e57480b62c187431761b
Camposlito <henrique_kuster14@hotmail.com>
2019-08-28 15:23:14 -0300

    commite all

--- save.php
+++ save.php
@@ -6,26 +6,13 @@ $email = $_POST["email"];
 $senha = $_POST["senha"];
 $nome = $_POST["nome"];
 
-if ($_SESSION["email"] == $email) {
-  $dados = array(
-    "nome" => $nome,
-    "senha" => $senha
-  );
-  $key = array(
-    'id' => $_SESSION["id_user"]
-  );
+$nova_senha = $_POST["nova-senha"];
+$r_nova_senha = $_POST["r-nova-senha"];
 
-  sqlUpdate("usuario", $dados, $key);
-
-  $_SESSION["senha"] = $senha;
-  $_SESSION["nome_user"] = $nome;
-  header("location:content.php?_location=settings&erro=null");
-}else {
-  if (temEmail($email) == null) {
+if ($_SESSION["senha"] == $nova_senha) {
+  if ($_SESSION["email"] == $email) {
     $dados = array(
-      "nome" => $nome,
-      "email" => $email,
-      "senha" => $senha
+      "nome" => $nome
     );
     $key = array(
       'id' => $_SESSION["id_user"]
@@ -33,12 +20,48 @@ if ($_SESSION["email"] == $email) {
 
     sqlUpdate("usuario", $dados, $key);
 
-    $_SESSION["email"] = $email;
-    $_SESSION["senha"] = $senha;
     $_SESSION["nome_user"] = $nome;
     header("location:content.php?_location=settings&erro=null");
+  }else {
+    if ($_SESSION["senha"] == $senha) {
+      if (temEmail($email) == null) {
+        $dados = array(
+          "email" => $email
+        );
+        $key = array(
+          'id' => $_SESSION["id_user"]
+        );
+
+        sqlUpdate("usuario", $dados, $key);
+
+        $_SESSION["email"] = $email;
+        header("location:content.php?_location=settings&erro=null");
+      }else {
+        header("location:content.php?_location=settings&erro=email");
+      }
+    }else{
+      header("location:content.php?_location=settings&erro=senha");
+    }
+  }
+}else{
+  if ($nova_senha == $r_nova_senha) {
+    if ($_SESSION["senha"] == $senha) {
+      $dados = array(
+        "senha" => $senha
+      );
+      $key = array(
+        'id' => $_SESSION["id_user"]
+      );
+
+      sqlUpdate("usuario", $dados, $key);
+
+      $_SESSION["senha"] = $senha;
+      header("location:content.php?_location=settings&erro=null");
+    }else {
+      header("location:content.php?_location=settings&erro=senha");
+    }
   }else{
-    header("location:content.php?_location=settings&erro=email");
+    header("location:content.php?_location=settings&erro=senha");
   }
 }
 
--- settings.php
+++ settings.php
@@ -6,65 +6,92 @@
   <div class="page-header">
     <h1>• &nbsp; <?php echo $_SESSION["nome_user"]; ?> &nbsp; • <span class="email-header">&nbsp; <?php echo $_SESSION["email"]; ?></span> </h1>
   </div>
-  <br><br><br>
+  <!-- ERRO DE CREDENCIAL SENHA -->
+  <?php if($_GET["erro"] == "senha"): ?>
+  <div class="erro-config">
+    <div class="alert alert-danger erro" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      <span class="glyphicon glyphicon-exclamation-sign erro-icon" aria-hidden="true"></span>
+      <span class="sr-only">Erro:</span>
+      &nbsp; Senha incorreta
+    </div>
+  </div>
+  <?php endif; ?>
+  <!-- ERRO DE CREDENCIAL EMAIL -->
+  <?php if($_GET["erro"] == "email"): ?>
+  <div class="erro-config">
+    <div class="alert alert-danger erro" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      <span class="glyphicon glyphicon-exclamation-sign erro-icon" aria-hidden="true"></span>
+      <span class="sr-only">Erro:</span>
+      &nbsp; E-mail já cadastrado
+    </div>
+  </div>
+  <?php endif; ?>
+  <br>
   <ul class="nav nav-pills nav-stacked config-new">
     <li role="presentation" onclick="appear(1)" class="pill active"><a href="#"> <span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp; Minha Conta</a></li>
     <li role="presentation" onclick="appear(2)" class="pill"><a href="#"> <span class="glyphicon glyphicon-credit-card" aria-hidden="true"></span>&nbsp; Alterar E-mail &nbsp; </a></li>
     <li role="presentation" onclick="appear(3)" class="pill"><a href="#"> <span class="glyphicon glyphicon-lock" aria-hidden="true"></span>&nbsp; Alterar Senha</a></li>
   </ul>
+
 <!-- formulário para editar nome -->
   <div class="main config-new nome">
-    <span>Minha Conta</span>
+    <div class="row">
+      <span>Minha Conta</span>
+    </div>
     <form class="" action="save.php" method="post">
       <div class="row">
-        <label for="nome" class="control-label">Nome</label>
-        <input type="text" name="nome" value="<?php echo $_SESSION["nome_user"]; ?>">
+        <input type="text" placeholder="Nome" name="nome" value="<?php echo $_SESSION["nome_user"]; ?>">
       </div>
+      <input type="hidden" name="nova-senha" value="<?php echo $_SESSION["senha"]; ?>">
+      <input type="hidden" name="r-nova-senha" value="<?php echo $_SESSION["senha"]; ?>">
       <input type="hidden" name="email" value="<?php echo $_SESSION["email"]; ?>">
       <input type="hidden" name="senha" value="<?php echo $_SESSION["senha"]; ?>">
       <div class="row">
-        <button type="submit">Salvar Alterações</button>
+        <button class="btn-salvar-alt btn-success" type="submit">Salvar Alterações</button>
       </div>
     </form>
   </div>
 <!-- formulário para editar email -->
   <div class="main config-new email">
-    <span>Alterar E-mail</span>
+    <div class="row">
+      <span>Alterar E-mail</span>
+    </div>
     <form class="" action="save.php" method="post">
+      <input type="hidden" name="nova-senha" value="<?php echo $_SESSION["senha"]; ?>">
+      <input type="hidden" name="r-nova-senha" value="<?php echo $_SESSION["senha"]; ?>">
       <input type="hidden" name="nome" value="<?php echo $_SESSION["nome_user"]; ?>">
       <div class="row">
-        <label for="email" class="control-label">E-mail</label>
-        <input type="text" name="email" value="<?php echo $_SESSION["email"]; ?>">
+        <input type="text" placeholder="E-mail" name="email" value="<?php echo $_SESSION["email"]; ?>">
       </div>
       <div class="row">
-        <label for="senha" class="control-label">Senha</label>
-        <input type="password" name="senha" value="">
+        <input type="password" placeholder="Senha" name="senha" value="">
       </div>
       <div class="row">
-        <button type="submit">Salvar Alterações</button>
+        <button class="btn-salvar-alt btn-success" type="submit">Salvar Alterações</button>
       </div>
     </form>
   </div>
 <!-- formulário para editar senha -->
   <div class="main config-new senha">
-    <span>Alterar Senha</span>
+    <div class="row">
+      <span>Alterar Senha</span>
+    </div>
     <form class="" action="save.php" method="post">
       <div class="row">
-        <label for="nova-senha">Nova Senha</label>
-        <input type="password" name="nova-senha" value="">
+        <input type="password" placeholder="Nova Senha" name="nova-senha" value="">
       </div>
       <div class="row">
-        <label for="r-nova-senha">Repetir Senha</label>
-        <input type="password" name="r-nova-senha" value="">
+        <input type="password" placeholder="Repetir Senha" name="r-nova-senha" value="">
       </div>
       <div class="row">
-        <label for="senha">Senha Atual</label>
-        <input type="password" name="senha" value="">
+        <input type="password" placeholder="Senha Atual" name="senha" value="">
       </div>
       <input type="hidden" name="nome" value="<?php echo $_SESSION["nome_user"]; ?>">
       <input type="hidden" name="email" value="<?php echo $_SESSION["email"]; ?>">
       <div class="row">
-        <button type="submit">Salvar Alterações</button>
+        <button class="btn-salvar-alt btn-success" type="submit">Salvar Alterações</button>
       </div>
     </form>
   </div>
